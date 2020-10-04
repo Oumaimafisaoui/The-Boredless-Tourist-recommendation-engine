36c8a5c02b484bc1fc02a488e9b54897bdb63db1
unknown <oumaimafisaoui@gmail.com>
2020-10-04 22:05:54 +0100

    add a function to add the index of destination

--- script.py
+++ script.py
@@ -4,3 +4,6 @@ destinations =['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Pau
 #This is a traveler (a user of The Boredless Tourist application) whose name is Erin Wilkes who likes historical buildings and art. Erin is in China right now, hopefully we can find some good places to show her.
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
