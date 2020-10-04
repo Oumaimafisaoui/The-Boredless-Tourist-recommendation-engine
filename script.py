f42ce0096099776068e2eae9fcfcf330cf2ceb28
unknown <oumaimafisaoui@gmail.com>
2020-10-04 21:21:54 +0100

    add testing user

---
+++ script.py
@@ -0,0 +1,6 @@
+#The first is our list of destinations that we’re going to be using.
+destinations =['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+#This is a traveler (a user of The Boredless Tourist application) whose name is Erin Wilkes who likes historical buildings and art. Erin is in China right now, hopefully we can find some good places to show her.
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
